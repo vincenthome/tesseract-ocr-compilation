1102c455bc56556278143c9f4fda78e172f828f1
Vincent Leung <vincenthome@hotmail.com>
2023-05-28 13:50:56 -0400

    test ocr against image using command line argument

--- scripts/4-test-ocr.sh
+++ scripts/4-test-ocr.sh
@@ -8,13 +8,17 @@ docker exec -it tesseract-ocr tesseract \--list-langs
 # docker exec -it tesseract-ocr tesseract \-h
 
 # Base OCR test
+image_url="https://github.com/vincenthome/tesseract-ocr-compilation/blob/master/test-images/problem.tif?raw=true" 
+if [ -n "$1" ]; then
+    image_url=$1  
+fi
 docker exec -it tesseract-ocr /bin/bash -c "echo ====; mkdir ./ocr-files/; cd ./ocr-files/; pwd; \
 rm phototest.*; echo ====; \
-echo downloading test file:; wget -O phototest.tif https://github.com/vincenthome/tesseract-ocr-compilation/blob/master/test-images/problem.tif?raw=true;  \
+echo downloading test file:; wget -O phototest.tif $image_url;  \
 tesseract phototest.tif phototest; \
 head -100 phototest.txt; \
 echo Compare the text above ^^^ with the source image: \ 
-echo https://github.com/vincenthome/tesseract-ocr-compilation/blob/master/test-images/problem.tif?raw=true"
+echo $image_url"
 
 # Process and copy files to ./ocr-files/ directory
 docker exec -it tesseract-ocr /bin/bash -c "cd ./ocr-files/; tesseract phototest.tif phototest -l eng --psm 1 --oem 2 pdf hocr"
